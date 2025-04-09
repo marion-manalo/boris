f796c438a069218973a6ae922c743d5f40321948
Jeremy Azucena <87372075+withers0@users.noreply.github.com>
2025-04-09 12:22:09 -0400

    added components for splash page

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,16 @@
+const Welcome = () => {
+    return (
+      <div className="text-center max-w-2xl mx-auto p-6">
+        <h1 className="text-3xl font-bold text-gray-800">
+          Need help with investing?
+        </h1>
+        <p className="text-lg text-gray-600 mt-4">
+          Get current insights on any stock and view recent company expense reports.
+          Enter a ticker to get started.
+        </p>
+      </div>
+    );
+  };
+  
+  export default Welcome;
+  
