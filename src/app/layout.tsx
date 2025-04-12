145fcebf5ee1bde99ac079159b0a6fd4190c7b73
Zachary Marino <167573843+gitzam1@users.noreply.github.com>
2025-04-12 12:03:16 -0400

    Import and add session provider to allow navbar component to use sessions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import "./globals.css";
+import {SessionProvider } from 'next-auth/react';
 
 
 export default function RootLayout({
@@ -10,8 +11,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <SessionProvider>
         <Navbar/>
         {children}
+        </SessionProvider>
       </body>
     </html>
   );
