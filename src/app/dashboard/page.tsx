a6d5b13098826fa8dec12ae59e308927c7b6ba84
Zachary Marino <167573843+gitzam1@users.noreply.github.com>
2025-04-17 11:13:55 -0400

    Update styling for splash, dash, searchbar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,7 +13,7 @@ export default function PublicDash() {
   }
 
   return (
-    <main className="publicdash-main">
+    <main>
       <div className="publicdash-container">
         <h1 className="publicdash-title">Welcome to the Stock Dashboard</h1>
         <p className="publicdash-p">  Welcome, { session?.user?.email || 'User'}.
